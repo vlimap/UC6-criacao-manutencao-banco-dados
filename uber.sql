91fc4e3e1d9cb22d14e8f616d4383b95847a80b8
Dev Flask <valtemir.procopio@gmail.com>
2024-03-13 13:20:25 -0300

    13/03/2024

--- uber.sql
+++ uber.sql
@@ -1,30 +1,52 @@
--- Criar meu banco de dados
-create database uber;
--- Usar o banco criado
-use ifood;
--- Criando a tabela cliente
-create table cliente(
-	nome nvarchar(50), 
+-- Criando  um banco
+create database escola;
+-- Usando o banco de dados
+use escola;
+
+create table aluno(
+	nome nvarchar(30),
 	cpf char(11) primary key
 );
+-- Inserindo registros na tabela aluno
+insert into aluno(nome, cpf)
+values
+--nome       cpf
+('Diego','12354567891'),
+('Matheus','98765432132'),
+('Rodolfo', '32145698714'),
+('Lucas', '14725836912')
 
-create table produto(
-	id_produto int primary key,
-	nome nchar(30)
-);
+-- Consultar registros da tabela aluno
+select * from aluno;
 
-create table pedido(
-	id_produto int,
+-- Criando a tabela turma_130
+create table turma_130(
+	nome nvarchar(30),-- Diego, Matheus
 	cpf char(11),
-	id_pedido int primary key
-	foreign key (id_produto) references produto(id_produto),
-	foreign key (cpf) references cliente(cpf),
+	foreign key (cpf) references aluno(cpf)
 );
 
+-- Criando a tabela turma_109
+create table turma_109(
+	nome nvarchar(30),-- Rodolfo, Lucas
+	cpf char(11),
+	foreign key (cpf) references aluno(cpf)
+);
 
+-- Alterando o nome da tabela
+exec sp_rename 'turma', 'turma_109';
 
+-- Forma alternativa
+-- Mudar no proprio SGBD 
 
+-- Alterar os nomes das colunas ou atributos
+exec sp_rename 'turma_109.nome','nome_aluno', 'COLUMN';
 
+-- Alterar os registros, alterando o nome do aluno onde o cpf �: 12354567891
+update aluno 
+set nome ='Diego Silva'
+where cpf='12354567891';
 
+select * from aluno;
 
 
