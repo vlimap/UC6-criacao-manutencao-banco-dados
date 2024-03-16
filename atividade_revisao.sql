6c7356d079d8d1ff52259d150e39f92ebf4ddfa9
Dev Flask <valtemir.procopio@gmail.com>
2024-03-16 08:14:36 -0300

    .

--- atividade_revisao.sql
+++ atividade_revisao.sql
@@ -6,28 +6,26 @@ select f.nome
 from funcionario f
 join gerente g on g.matricula = f.matricula;
 
--- Selecionar todos os livros com ano de publicação acima de 2018
+-- Selecionar todos os livros com ano de publicaÃ§Ã£o acima de 2018
 select * from livro where ano_publicacao > 2018;
-select * from livro
--- Titulo do livro onde o nome do autor começa com Paulo
+select * from livro;
+-- TÃ­tulo do livro onde o nome do autor comeÃ§a com Paulo
 select titulo from livro where autor like 'paulo%';
--- Mostre todos os livros que são de tecnologia
+-- Mostre todos os livros que sÃ£o de tecnologia
 select livro.titulo
 from livro
 join tecnologia on tecnologia.numero_registro = livro.numero_registro;
 
-
 select column_name, data_type
 from information_schema.COLUMNS
 where TABLE_NAME = 'livro';
-
 
 select * from evento;
 
 select * from evento 
 where tipo = 'palestra' 
 and custo > 100.00
-and year(data) > 2022; 
+and year(data) > 2022;
 
 -- Operadores relacionais
 -- = (igual a)
@@ -37,43 +35,43 @@ and year(data) > 2022;
 -- >= (maior ou igual a)
 -- <= (menor ou igual a)
 
- -- Operadores lógicos
--- AND: Retorna verdadeiro se ambas as condições forem verdadeiras.
--- OR: Retorna verdadeiro se pelo menos uma das condições for verdadeira.
--- NOT: Inverte o resultado de uma condição, ou seja, retorna verdadeiro se a condição for falsa e vice-versa.
+-- Operadores lÃ³gicos
+-- AND: Retorna verdadeiro se ambas as condiÃ§Ãµes forem verdadeiras.
+-- OR: Retorna verdadeiro se pelo menos uma das condiÃ§Ãµes for verdadeira.
+-- NOT: Inverte o resultado de uma condiÃ§Ã£o, ou seja, retorna verdadeiro se a condiÃ§Ã£o for falsa e vice-versa.
 
--- Operadores aritmeticos
--- + (adição): Soma dois valores.
--- - (subtração): Subtrai um valor de outro.
--- * (multiplicação): Multiplica dois valores.
--- / (divisão): Divide um valor pelo outro.
--- % (módulo): Retorna o resto da divisão de um valor pelo outro.
+-- Operadores aritmÃ©ticos
+-- + (adiÃ§Ã£o): Soma dois valores.
+-- - (subtraÃ§Ã£o): Subtrai um valor de outro.
+-- * (multiplicaÃ§Ã£o): Multiplica dois valores.
+-- / (divisÃ£o): Divide um valor pelo outro.
+-- % (mÃ³dulo): Retorna o resto da divisÃ£o de um valor pelo outro.
 
--- Funções de agregação:
+-- FunÃ§Ãµes de agregaÃ§Ã£o:
 -- SUM(): Calcula a soma dos valores em um conjunto de dados.
--- AVG(): Calcula a média dos valores em um conjunto de dados.
--- COUNT(): Conta o número de itens em um conjunto de dados.
--- MAX(): Encontra o valor máximo em um conjunto de dados.
--- MIN(): Encontra o valor mínimo em um conjunto de dados.
+-- AVG(): Calcula a mÃ©dia dos valores em um conjunto de dados.
+-- COUNT(): Conta o nÃºmero de itens em um conjunto de dados.
+-- MAX(): Encontra o valor mÃ¡ximo em um conjunto de dados.
+-- MIN(): Encontra o valor mÃ­nimo em um conjunto de dados.
 
--- Funções escalares:
--- ABS(): Retorna o valor absoluto de um número.
--- CEILING(): Arredonda um número para cima para o inteiro mais próximo.
--- FLOOR(): Arredonda um número para baixo para o inteiro mais próximo.
--- ROUND(): Arredonda um número para um número especificado de casas decimais.
--- SQRT(): Calcula a raiz quadrada de um número.
--- POWER(): Eleva um número a uma potência especificada.
+-- FunÃ§Ãµes escalares:
+-- ABS(): Retorna o valor absoluto de um nÃºmero.
+-- CEILING(): Arredonda um nÃºmero para cima para o inteiro mais prÃ³ximo.
+-- FLOOR(): Arredonda um nÃºmero para baixo para o inteiro mais prÃ³ximo.
+-- ROUND(): Arredonda um nÃºmero para um nÃºmero especificado de casas decimais.
+-- SQRT(): Calcula a raiz quadrada de um nÃºmero.
+-- POWER(): Eleva um nÃºmero a uma potÃªncia especificada.
 
---Funções de manipulação de strings:
+-- FunÃ§Ãµes de manipulaÃ§Ã£o de strings:
 -- LEN(): Retorna o comprimento de uma string.
--- UPPER(): Converte uma string para maiúsculas.
--- LOWER(): Converte uma string para minúsculas.
+-- UPPER(): Converte uma string para maiÃºsculas.
+-- LOWER(): Converte uma string para minÃºsculas.
 -- SUBSTRING(): Extrai uma substring de uma string.
--- REPLACE(): Substitui todas as ocorrências de uma substring por outra substring.
+-- REPLACE(): Substitui todas as ocorrÃªncias de uma substring por outra substring.
 
---Funções de manipulação de datas:
+-- FunÃ§Ãµes de manipulaÃ§Ã£o de datas:
 -- GETDATE(): Retorna a data e hora atuais do sistema.
 -- DATEADD(): Adiciona um intervalo de tempo especificado a uma data.
--- DATEDIFF(): Calcula a diferença entre duas datas.
+-- DATEDIFF(): Calcula a diferenÃ§a entre duas datas.
 -- YEAR(): Extrai o ano de uma data.
--- MONTH(): Extrai o mês de uma data.
+-- MONTH(): Extrai o mÃªs de uma data.
